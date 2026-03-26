173751c9009750bd21816e57ee2412ae85c29392
mohit-logicrays <mohit.prajapat@logicrays.com>
2026-03-26 17:41:18 +0530

    feat: Add base Python dependencies, enable default `IsAuthenticated` permissions, from Django REST Framework settings.

--- settings/rest_framework.py
+++ settings/rest_framework.py
@@ -3,9 +3,9 @@ from django.utils.timezone import timedelta
 # Rest Framework Configuration
 # https://www.django-rest-framework.org/
 REST_FRAMEWORK = {
-    # "DEFAULT_PERMISSION_CLASSES": [
-    #     "rest_framework.permissions.IsAuthenticated",
-    # ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 20,
     "DEFAULT_AUTHENTICATION_CLASSES": [
